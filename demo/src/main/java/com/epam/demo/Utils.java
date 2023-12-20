959c14fa88c246e184b088f20ec2e2674ec49e1a
VLinkus <LinkusVyte@gmail.com>
2023-12-20 16:17:49 +0200

    Gradle

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        return !args.stream().anyMatch(x -> {
+            int number = -1;
+            try {
+                number = Integer.parseInt(x);
+            } catch (NumberFormatException e) {
+                System.out.println(e);
+            }
+            return number < 0;
+        });
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,15 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        boolean isPositive = false;
+        if(!str.isEmpty()) {
+            try {
+                int number = Integer.parseInt(str);
+                isPositive = number > 0;
+            } catch (NumberFormatException e) {
+                System.out.println(e);
+            }
+        }
+        return isPositive;
     }
 }
